eaf399b1b1e1a4fe9dc086f615d4f019dd11de4f
amaliejacobsen <amajac@norlys.dk>
2026-04-20 09:54:59 +0200

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://ehrbiwdyqypkxzdknuin.supabase.co',
+  'sb_publishable_VOFoizytcjat0ikSJiaJPA_bVZZPxkU'
+)
